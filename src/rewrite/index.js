b857d4f49e575abf602812bfa0abc8924bcd2e35
THEBESTSI <angelalbeach@gmail.com>
2025-05-08 17:17:00 -0400

    Update index.js

--- src/rewrite/index.js
+++ src/rewrite/index.js
@@ -183,6 +183,7 @@ class Ultraviolet {
 
 export default Ultraviolet;
 if (typeof self === "object") self.Ultraviolet = Ultraviolet;
+
 const PORT = process.env.PORT || 3000;
 
 app.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
